9a62564b023c933a52d92eff7a1e959be551db94
BurjuYuksel <burcuyuksel.t@gmail.com>
2021-08-14 21:07:16 -0400

    first commit

---
+++ Task6.java
@@ -0,0 +1,31 @@
+
+public class Task6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+// TASK 6 // Write a Java Program to print the first 10 numbers of Fibonacci series.
+		
+		// 0 1 1 2 3 5 8 13 21 34 
+		
+		
+		int a,b,c; 
+		a=0;
+		b=1;
+		   
+		 for(int i=0;i<=10;i++)  {  
+			 
+		System.out.println (a +" " );
+		  
+			 c=a+b;
+			 a=b;
+			 b=c;
+		 	 
+		 
+			 
+		 }
+		 
+	}
+
+}
---
+++ Task8.java
@@ -0,0 +1,43 @@
+
+public class Task8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+
+// TASK 8 // Write a java program to find the second largest number in the array?
+		
+		
+		int arr[] = { 1,2,3,4,5,6,7,8,9 };
+		
+		  int largest = arr[0];
+		   
+		  int secondLargest = arr[0];
+		  
+		
+		  for (int i = 0; i < arr.length; i++) {
+		
+			  if (arr[i] > largest) {
+				  
+				  largest = arr[i];
+				  
+				  
+			  } else if (arr[i] > secondLargest) {
+				   	 
+				    secondLargest = arr[i];	  
+				  
+			  }
+			  
+			 
+		  }		
+			 	
+		  System.out.println("\nSecond largest number is:" + secondLargest);
+	
+	
+		  
+		  
+		  
+	}
+
+}
